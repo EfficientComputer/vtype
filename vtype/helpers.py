d16feeebc18c29694ad90c6e5c3edb66f27039d5
Graham Gobieski <graham.gobieski@gmail.com>
2023-03-20 14:13:34 -0400

    [helpers] added ability of helpers to instantiate type when given a type

--- vtype/helpers.py
+++ vtype/helpers.py
@@ -1,10 +1,16 @@
 def bits(typ):
+    if type(typ) is type:
+        typ = typ()
     return typ.width()
 
 
 def bitwidth(v):
+    if type(v) is type:
+        v = v()
     return int(v).bit_length()
 
 
 def clog2(v):
+    if type(v) is type:
+        v = v()
     return int(v - 1).bit_length()
